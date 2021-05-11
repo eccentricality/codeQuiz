f031327ed0d45a415972ea1c9eaa42faa629de00
eccentricality <youngmin13@gmail.com>
2021-05-11 16:49:19 -0400

    coded question generation

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,32 @@
+const startButton = document.getElementById("startButton")
+const quizRules = document.getElementById("quizRules")
+const qrQuitButton = document.getElementById("quitButton")
+const qrContinueButton = document.getElementById("continueButton")
+const quizBody = document.getElementById("quizBody")
+const questionText = document.getElementById("questions");
+const answerA = document.getElementById("answerA");
+const answerB = document.getElementById("answerB");
+const answerC = document.getElementById("answerC");
+const answerD = document.getElementById("answerD");
 
+// on click of start button transitions quiz rules in by adding class that reveals
+startButton.onclick = ()=>{
+    quizRules.classList.add("activateDivs");
+}
+
+// on click goes back to the quiz start button
+qrQuitButton.onclick = ()=>{
+    quizRules.classList.remove("activateDivs");
+}
+
+// on click starts the quiz with first question
+qrContinueButton.onclick = ()=>{
+    quizRules.classList.remove("activateDivs");
+    quizBody.classList.add("activateDivs");
+    retrieveQuestion();
+}
+
+// -------------------------QUIZ BANK BEGIN----------------------------------------
 
 const quizBank = [
     {
@@ -32,6 +60,24 @@ const quizBank = [
     
 ]
 
+// -------------------------END OF QUIZ BANK-------------------------------------
+
+let numberOfQuestions = 0;
+
+// retrieve questions from array of stored question objects and innerText to display in body
+function retrieveQuestion(){
+    let retrievedQuestion = quizBank[0].question;
+    let retrievedAnswerA = quizBank[0].answers;
+    let retrievedAnswerB = quizBank[0].answers[1];
+    let retrievedAnswerC = quizBank[0].answers[2];
+    let retrievedAnswerD = quizBank[0].answers[3];
+
+    questionText.innerText = retrievedQuestion;
+    answerA.innerText = retrievedAnswerA;
+    answerB.innerText = retrievedAnswerB;
+    answerC.innerText = retrievedAnswerC;
+    answerD.innerText = retrievedAnswerD;
+}
 
 // 1. start button: when clicked it starts timer and shows first question
 // 2. list items for answer selections: when answer is chosen reveal correctness of answer
