3e551f2e0d18c1796c9e074db93193635ca3c0a3
eccentricality <youngmin13@gmail.com>
2021-05-11 22:27:39 -0400

    coded functions to move from question to question until no more questions

--- assets/script.js
+++ assets/script.js
@@ -4,10 +4,12 @@ const qrQuitButton = document.getElementById("quitButton")
 const qrContinueButton = document.getElementById("continueButton")
 const quizBody = document.getElementById("quizBody")
 const questionText = document.getElementById("questions");
+const answers = document.getElementById("answers");
 const answerA = document.getElementById("answerA");
 const answerB = document.getElementById("answerB");
 const answerC = document.getElementById("answerC");
 const answerD = document.getElementById("answerD");
+const totalQuestions = document.getElementById("totalQuestions");
 
 // on click of start button transitions quiz rules in by adding class that reveals
 startButton.onclick = ()=>{
@@ -23,13 +25,81 @@ qrQuitButton.onclick = ()=>{
 qrContinueButton.onclick = ()=>{
     quizRules.classList.remove("activateDivs");
     quizBody.classList.add("activateDivs");
-    retrieveQuestion();
+    retrieveQuestion(0);
 }
+
+let quizBankIndex = 0;
+
+// changes color back to original on wrong answer
+answers.addEventListener("click", function(){
+    var self = quizBody;
+        oldBg = quizBody.style.background;
+    setTimeout(function(){
+        self.style.background = oldBg;
+    }, 150);
+})
+
+// on choosing an answer display the next question and also flash red if answer is wrong
+answers.onclick = ()=>{
+    if(quizBankIndex < quizBank.length -1){
+        quizBankIndex++;
+        retrieveQuestion(quizBankIndex);
+    }
+    else{
+        console.log("QUIZ COMPLETE")
+    }
+}
+
+// change screen color when wrong answer to change back for flashing effect
+function chgColorWrong(){
+    quizBody.style.backgroundColor = "red";
+}
+
+function chgColorRight(){
+    quizBody.style.backgroundColor = "green";
+}
+
+// retrieve questions from array of stored question objects and innerText to display in body
+function retrieveQuestion(index){
+    let retrievedQuestion = "[" + quizBank[index].number + "] " + quizBank[index].question;
+    let retrievedAnswerA = quizBank[index].answers.a;
+    let retrievedAnswerB = quizBank[index].answers.b;
+    let retrievedAnswerC = quizBank[index].answers.c;
+    let retrievedAnswerD = quizBank[index].answers.d;
+
+    // prints object definition into proper divs to present question and answer choices
+    questionText.innerText = retrievedQuestion;
+    answerA.innerText = retrievedAnswerA;
+    answerB.innerText = retrievedAnswerB;
+    answerC.innerText = retrievedAnswerC;
+    answerD.innerText = retrievedAnswerD;
+    // keeps track of question count
+    totalQuestions.innerText = quizBank[index].number + " out of " + quizBank.length + " Questions"
+
+    // stores selected answer to be compared for correctness
+    const selected = 
+    for(i=0; i < quizBank[index]; i++){
+
+    }
+}
+
+// check if user answer is correct
+function checkAnswer(answer);
+    let chosenAnswer = answer.innerText;
+    let correctAnswer = quizBank[quizBankIndex].correctAnswer;
+    console.log(chosenAnswer);
+    console.log(correctAnswer);
+
+// 5. when question is answered incorrectly subtract time from timer
+// 6. if all questions are answered correctly or timer hits 0 game is over
+// 7. when game is over present input for option to store score for local storage
+
 
 // -------------------------QUIZ BANK BEGIN----------------------------------------
 
 const quizBank = [
     {
+        number: "1",
         question: "JavaScript is a _____-side programming language.",
         answers: {
             a: "Client",
@@ -40,14 +110,18 @@ const quizBank = [
         correctAnswer: "c"
     },
     {
+        number: "2",
         question: "If you type the following code in the console window, what result will you get?\n'3 > 2 > 1 === false;'",
         answers: {
             a: "True",
-            b: "False"
+            b: "False",
+            c: null,
+            d: null
         },
         correctAnswer: "b"
     },
     {
+        number: "3",
         question: "Which of the following will write the message 'Hello DataFlair!' in an alert box?",
         answers: {
             a: "alertBox('Hello DataFlair!');",
@@ -61,28 +135,3 @@ const quizBank = [
 ]
 
 // -------------------------END OF QUIZ BANK-------------------------------------
-
-let numberOfQuestions = 0;
-
-// retrieve questions from array of stored question objects and innerText to display in body
-function retrieveQuestion(){
-    let retrievedQuestion = quizBank[0].question;
-    let retrievedAnswerA = quizBank[0].answers;
-    let retrievedAnswerB = quizBank[0].answers[1];
-    let retrievedAnswerC = quizBank[0].answers[2];
-    let retrievedAnswerD = quizBank[0].answers[3];
-
-    questionText.innerText = retrievedQuestion;
-    answerA.innerText = retrievedAnswerA;
-    answerB.innerText = retrievedAnswerB;
-    answerC.innerText = retrievedAnswerC;
-    answerD.innerText = retrievedAnswerD;
-}
-
-// 1. start button: when clicked it starts timer and shows first question
-// 2. list items for answer selections: when answer is chosen reveal correctness of answer
-// 3. keep track of the number of correct answers to be recalled for final score
-// 4. when question is answered correctly replace question with new question
-// 5. when question is answered incorrectly subtract time from timer
-// 6. if all questions are answered correctly or timer hits 0 game is over
-// 7. when game is over present input for option to store score for local storage
